1fdeaaad11ccac099835054d2e7176e3948409cb
Juan Embid <juan.embid@gmail.com>
2022-09-16 11:43:36 +0200

    Dalton
    Correct

--- 314/314-hemanos-dalton.cpp
+++ 314/314-hemanos-dalton.cpp
@@ -11,14 +11,31 @@ const int MAX_HERMANOS = 100000;
 
 bool compruebaHermanos(const int hermanos[], int cantidad) {
     int i = 0;
-    bool dalton = true;
-    while ((i < cantidad) && dalton) {
-        if(i + 1 < cantidad)
-            dalton = (hermanos[i] < hermanos[i + 1]);
-        i++;
+    bool dalton = (hermanos[i] < hermanos[i + 1]);
+
+    // Asciende
+    if (dalton) {
+        while ((i < cantidad) && dalton) {
+            if(i + 1 < cantidad)
+                dalton = (hermanos[i] < hermanos[i + 1]);
+            i++;
+        }
+        if(i == cantidad)
+            return true;
     }
 
-    return dalton;
+    // Desciende
+    else {
+        while ((i < cantidad) && !dalton) {
+            if(i + 1 < cantidad)
+                dalton = (hermanos[i] <= hermanos[i + 1]);
+            i++;
+        }
+        if(i == cantidad)
+            return true;
+    }
+
+    return false;
 }
 
 bool resuelveCaso() {
