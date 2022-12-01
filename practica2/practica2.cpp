c8923671004042ac538ca2fe3822e13d477abab7
Juan Embid <juan.embid@gmail.com>
2022-12-01 14:34:00 +0100

    Run error

--- practica2/practica2.cpp
+++ practica2/practica2.cpp
@@ -8,12 +8,32 @@
 using namespace std;
 
 //Implementa aqui tu funcion recursiva
-
-int resolver(const vector<int>&v)
+// Llamada inicial con c = 0 y f = v.size() - 1 (intervalo cerrado) donde c es el comienzo y f el final del segmento
+int resolver(const vector<int>& v, int c, int f){
+    int m;
+    if (c == f) { // CB: un elemento
+        return v.at(f);
+    } else if(f - c == 1) { // CB: dos elementos
+        if (v.at(c) % 2 != 0) return v.at(c);
+        return v.at(f);
+    } else { // CR
+        m = (c + f) / 2;
+        if ((v.at(m) - v.at(c)) / 2 != m - c) { // elemento impar en izquierda
+            resolver(v, c, m);
+        } else if ((v.at(f) - v.at(m + 1)) / 2 != f - m + 1) { // elemento impar en derecha
+            resolver(v, m + 1, f);
+        }
+    }
+}
 
 
 //Analiza el coste: escribe la recurrencia y el orden de complejidad al que pertenece la solucion a la misma
-
+/* RECURRENCIA: Sea n el tamaño del vector: n = f - c + 1
+    T(n) = c si n = 1
+    T(n) = 2T(n/2) + c1 si n >= 2
+    T(n) pertence a O(n)
+    COSTE: En el caso peor es O(n) donde n es v.size()
+*/
 
 
 // Resuelve un caso de prueba, leyendo de la entrada la
@@ -29,8 +49,9 @@ bool resuelveCaso()
         std::cin >> v[i];
     int impar;
     // Aqui la llamada a la funcion resolver
+    int c = 0, f = numElem - 1;
+    impar = resolver(v, c, f);
     
-    //...
     // Aqui escribir los resultados
     std::cout << impar <<endl;
     
